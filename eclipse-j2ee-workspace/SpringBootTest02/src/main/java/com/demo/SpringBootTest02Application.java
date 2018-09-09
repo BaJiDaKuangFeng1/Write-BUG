ee81704775375195cd4ae1f88b4dac920622f39a
hubing <286735396@qq.com>
2018-09-09 19:25:26 +0800

    上传项目

---
+++ eclipse-j2ee-workspace/SpringBootTest02/src/main/java/com/demo/SpringBootTest02Application.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTest02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTest02Application.class, args);
+	}
+}
